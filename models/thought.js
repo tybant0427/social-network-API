1861c9ed86189811bcd871b2b373497a3b1ca961
Tyler Bantley <tyler.bantley@icloud.com>
2022-02-15 21:11:38 -0600

    thought model/schema build

---
+++ models/thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Schema, Types } = require('mongoose');
+//const dateFormat = require('../utils/dateFormat');
 
 const userSchema = new Schema(
     {
