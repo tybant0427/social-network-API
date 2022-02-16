6764d933ee1dbab33b75200192a68a4b6f4a9f85
Tyler Bantley <tyler.bantley@icloud.com>
2022-02-15 21:25:11 -0600

    added model index

--- models/thought.js
+++ models/thought.js
@@ -28,7 +28,7 @@ const thoughtSchema = new Schema(
     }
 )
 
-ThoughtSchema.virtual('reactionCount').get(function () {
+thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
 
