92e88f2657b332733fd25a3bdb84d1a15cf58617
Tyler Bantley <tyler.bantley@icloud.com>
2022-02-15 21:00:34 -0600

    building user model

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [
+            /.+@.+\..+/,
+          "Enter a valid email",
+        ],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  
+  userSchema.virtual('commentCount').get(function () {
+    return this.friends.length;
+  });
+  
+const User = model("User", userSchema);
+  
+module.exports = User;
